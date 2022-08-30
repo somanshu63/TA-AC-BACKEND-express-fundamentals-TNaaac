bc3a778671ff5cfc1d70ddff4580d0e025256197
somanshu63 <somanshu63@gmail.com>
2022-08-30 17:09:18 +0530

    express fundamentals assignment 2

--- block-BNaabw/server.js
+++ block-BNaabw/server.js
@@ -7,7 +7,7 @@ var app = express();
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 app.use(logger('dev'));
 app.use(cookieParser());
 
